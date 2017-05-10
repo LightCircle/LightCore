825f4ec5b79dc6dd7918b203fb30cb85a38a7222
lilin <1qaz2wsx>
2017-05-10 17:56:29 +0800

    修改language空时，判断出错的BUG 升级encrypt方法与python采用相同方式

--- test/test_security.js
+++ test/test_security.js
@@ -2,21 +2,39 @@
  * @file test security
  */
 
-"use strict";
+'use strict';
 
-const should  = require("should")
-  , security  = require("../lib/security")
+const should  = require('should')
+  , security  = require('../lib/security')
 ;
 
-describe("/lib/security", function () {
+describe('/lib/security', function () {
 
   /** *************************************** **/
-  describe("exports.encrypt", function () {
+  describe('exports.encrypt', function () {
 
-    it("encrypt", function (done) {
+    it('encrypt', function (done) {
 
       console.log(security.encrypt('1234567890123456', '1234567890123456'));
       console.log(security.decrypt('7a91d5ee4bbdc2f3bea9913118855b8f', 'light'));
+
+      done();
+    });
+
+    it('encrypt2', function (done) {
+
+      console.log(security.encrypt2('2e35501c2b7e', 'light'));
+
+      done();
+    });
+  });
+
+  /** *************************************** **/
+  describe('exports.decrypt', function () {
+
+    it('decrypt2', function (done) {
+
+      console.log(security.decrypt2('SPvPSa3cTKdfLgE7hKh0Pw==', 'light'));
 
       done();
     });
