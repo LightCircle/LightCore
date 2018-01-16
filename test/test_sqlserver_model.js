1556d2822827bbe189a1a26afb038b038867a949
LI LIN <r2space@gmail.com>
2018-01-16 17:20:39 +0800

    sqlserver 的 ctrl 和 model 初版 v1

--- test/test_sqlserver_model.js
+++ test/test_sqlserver_model.js
@@ -44,20 +44,78 @@ describe('/lib/mongo/controller', function () {
   //   });
   // });
 
-  describe('list', function () {
-    it('list', function (done) {
+  describe('query', function () {
 
-      const query = 'SELECT [Sid],[Expires],[Sess] FROM [dbo].[sessions]'
+    // it('list', function (done) {
+    //
+    //   const query = 'SELECT xml.query(\'/root/item\') AS A, xml.query(\'/root[item=3]\') AS B, [_id], [createAy] FROM [dbo].[xml] WHERE [_id] = \'<%- _id %>\''
+    //     , params  = {_id: 'x123456789'};
+    //
+    //   new model(domain, code, table, options).list(query, params, (err, result) => {
+    //     console.log(err, result);
+    //     done();
+    //   });
+    // });
+
+    // it('add', function (done) {
+    //
+    //   const data = {
+    //     _id     : 'x123456782',
+    //     xml     : '<root><a>1</a><b>2</b></root>',
+    //     createAy: '2012-01-01 10:00:00.000'
+    //   };
+    //
+    //   const query = 'INSERT INTO [dbo].[xml] ([xml],[_id], [createAy]) VALUES (\'<%- data._id %>\',\'<%- data.xml %>\',\'<%- data.createAy %>\')';
+    //
+    //   new model(domain, code, table, options).add(query, data, (err, result) => {
+    //     console.log(err, result);
+    //     done();
+    //   });
+    // });
+
+    // it('update', function (done) {
+    //
+    //   const data = {
+    //     xml     : '<root><a>1</a><b>2</b></root>',
+    //     createAy: '2018-01-01 10:00:00.000'
+    //   };
+    //
+    //   const condition = {
+    //     _id: 'x123456782'
+    //   };
+    //
+    //   const query = 'UPDATE [dbo].[xml] ' +
+    //     'SET [xml] = \'<%- data.xml %>\',[createAy] = \'<%- data.createAy %>\' ' +
+    //     'WHERE [_id] = \'<%- condition._id %>\'';
+    //
+    //   new model(domain, code, table, options).update(query, data, condition, (err, result) => {
+    //     console.log(err, result);
+    //     done();
+    //   });
+    // });
+
+    // it('get', function (done) {
+    //
+    //   const query = 'SELECT xml.query(\'/root/item\') AS A, xml.query(\'/root[item=3]\') AS B, [_id], [createAy] FROM [dbo].[xml] WHERE [_id] = \'<%- _id %>\''
+    //     , params  = {_id: 'x123456789'};
+    //
+    //   new model(domain, code, table, options).get(query, params, (err, result) => {
+    //     console.log(err, result);
+    //     done();
+    //   });
+    // });
+
+    it('count', function (done) {
+
+      const query = 'SELECT COUNT(1) AS COUNT FROM [dbo].[xml]'
         , params  = {};
 
-      new model(domain, code, table, options).list(query, params, (err, result) => {
-
+      new model(domain, code, table, options).count(query, params, (err, result) => {
         console.log(err, result);
-
-        // result.should.be.eql('alphabets');
         done();
       });
     });
+
   });
 
 });
